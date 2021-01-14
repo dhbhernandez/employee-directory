8d93c0b72a4fa52e434a5db37d439265cedd275d
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2021-01-13 20:53:57 -0600

    fnish funtionality, bootstrap table, search and jumbotron

--- src/components/OmdbContainer.js
+++ src/components/OmdbContainer.js
@@ -10,7 +10,8 @@ import API from "../utils/API";
 class OmdbContainer extends Component {
   state = {
     result: [],
-    search: ""
+    search: "",
+    sort: true
   };
 
   // When this component mounts, search for the movie "The Matrix"
@@ -41,19 +42,105 @@ class OmdbContainer extends Component {
   };
 
   handleLastName = () => {
-    console.log('result',this.state.result)
+    console.log('result', this.state.result)
+
+
+    let newList;
+
+    if (this.state.sort) {
+      //  block of code to be executed if the condition is true
+      newList = this.state.result.sort(this.compareAZ)
+    } else {
+      //  block of code to be executed if the condition is false
+      newList = this.state.result.sort(this.compareZA)
+    }
+
+    console.log('newList', newList);
+
+    this.setState({ result: newList, sort: !this.state.sort });
+
   }
+
+  //sort a-z
+  compareAZ = (a, b) => {
+
+    // console.log('a', a)
+    // console.log('b', b)
+    if (a.name.last < b.name.last) {
+      return -1;
+    }
+    if (a.name.last > b.name.last) {
+      return 1;
+    }
+    return 0;
+  }
+
+  compareZA = (a, b) => {
+    if (a.name.last > b.name.last) {
+      return -1;
+    }
+    if (a.name.last < b.name.last) {
+      return 1;
+    }
+    return 0;
+  }
+
+
+
+  // objs.sort( compare );
+
 
   render() {
 
     console.log('state', this.state.result)
     return (
-      <Container>
-        <table>
+
+      <>
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Employee Directory</h1>
+            <p class="lead">Search or sort employee by last name.
+            .</p>
+          </div>
+        </div>
+        <input className="form-control mr-sm-2" onChange={this.handleLastName} placeholder="search"></input>
+        
+        <Container>
+          <table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Photo</th>
+                <th scope="col">First Name</th>
+                <th scope="col" onClick={this.handleLastName}>Last Name</th>
+                <th scope="col">Age</th>
+              </tr>
+
+
+
+            </thead>
+            <tbody>
+              {this.state.result.map(contact => (
+
+                <tr>
+                  {/* <th scope="row">1</th> */}
+                  <td><img src={contact.picture.thumbnail} /></td>
+                  <td>{contact.name.first}</td>
+                  <td>{contact.name.last}</td>
+                  <td>{contact.dob.age}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+         
+         
+
+
+          {/* <table>
           <tr>
             <th>Photo</th>
-            <th>Firstname</th>
-            <th onClick={this.handleLastName}>Lastname</th>
+            <th>First Name</th>
+            <th onClick={this.handleLastName}>Last Name</th>
             <th>Age</th>
           </tr>
 
@@ -66,12 +153,12 @@ class OmdbContainer extends Component {
             </tr>
 
           ))}
-        </table>
+        </table> */}
 
 
 
 
-        {/* <Row>
+          {/* <Row>
           <Col size="md-8">
             <Card
               heading={this.state.result.Title || "Search for a Movie to Begin"}
@@ -99,7 +186,8 @@ class OmdbContainer extends Component {
             </Card>
           </Col>
         </Row> */}
-      </Container>
+        </Container>
+      </>
     );
   }
 }
